3f1d8c4d4f4f873f8c980b4eadfe609c881e4b07
Alamin <pg2@banglafire.com>
2018-07-02 13:07:43 +0600

    update student login

--- Software/studentLogin.c
+++ Software/studentLogin.c
@@ -19,20 +19,37 @@ void student_main_menu ()
     printf("\t\t\t    1.Summery / 2.Mark Sheet / 3.Total Course / 4.Exit\n\n");
     printf("\t\t---------------------------------------------------------------------------------------------\n\n");
 
-    int select_menu;   printf("\t\t\t\t\tPress the Number of Menu: ");   scanf("%d",&select_menu);
+    int select_menu;
+    printf("\t\t\t\t\tPress the Number of Menu: ");
+    scanf("%d",&select_menu);
 
     switch(select_menu)
     {
-        case 1:     system("CLS");  title();    viewSummery();    break;
+    case 1:
+        system("CLS");
+        title();
+        viewSummery();
+        break;
 
-        case 2:     system("CLS");  title();    markSheet();  break;
+    case 2:
+        system("CLS");
+        title();
+        markSheet();
+        break;
 
 
-        case 3:     system("CLS");  title();    totalCourse();  break;
+    case 3:
+        system("CLS");
+        title();
+        totalCourse();
+        break;
 
-        case 4:     break;
+    case 4:
+        break;
 
-        default:    printf("\t\t\t\tPress select the right menu");  break;
+    default:
+        printf("\t\t\t\tPress select the right menu");
+        break;
     }
     exit(1);
 }
@@ -45,7 +62,8 @@ void login()
     printf("\n\n\n\n\n\t\t\t\t\t\t\tStudent Login Panel\n");
     printf("\t\t\t\t\t-----------------------------------------------\n\n");
 
-    printf("\t\t\t\t\t\tEnter ID\t:   ");         scanf("%d",&id);
+    printf("\t\t\t\t\t\tEnter ID\t:   ");
+    scanf("%d",&id);
 
     f = fopen("text/student.txt","r");
     while(!feof(f))
@@ -53,9 +71,11 @@ void login()
         fscanf(f,"%d %[^,], %d %d %s %s %[^\n]",&read_student.id,read_student.name,&read_student.section,&read_student.intake,read_student.shift,read_student.program,read_student.semester);
         if(id == read_student.id)
         {
-            match = 1;  break;
+            match = 1;
+            break;
         }
-    }fclose(f);
+    }
+    fclose(f);
 
     if(match == 1)
     {
@@ -66,58 +86,75 @@ void login()
             if(id == readID)
             {
                 match2 = 1;
-                printf("\n\n\t\t\t\t\t\tEnter Password\t:   ");  scanf("%s",pass);
+                printf("\n\n\t\t\t\t\t\tEnter Password\t:   ");
+                scanf("%s",pass);
                 if(strcmp(pass,readPass) == 0)
                 {
                     strcpy(StuNameTitle,read_student.name);
                     StudentID = read_student.id;
-                    system("CLS");  title();    student_main_menu();
+                    system("CLS");
+                    title();
+                    student_main_menu();
                     break;
                 }
                 else
                 {
                     printf("\n\t\t\t\t\t-----------------------------------------------\n\n");
-                    printf("\t\t\t\t\t!!! Incorrect Password !!!, Try Again ? y / n : "); permission = getch();
+                    printf("\t\t\t\t\t!!! Incorrect Password !!!, Try Again ? y / n : ");
+                    permission = getch();
 
                     if(permission == 'Y' || permission == 'y')
                     {
-                        system("CLS");    login();
+                        system("CLS");
+                        login();
                     }
                     else
                         exit(1);
                 }
             }
-        }fclose(f);
+        }
+        fclose(f);
 
         if(match2 == 0)
         {
-            printf("\n\n\t\t\t\t\t\tCreate Password\t:   ");  scanf("%s",pass);
+            printf("\n\n\t\t\t\t\t\tCreate Password\t:   ");
+            scanf("%s",pass);
             printf("\n\t\t\t\t\t-----------------------------------------------\n\n");
-            printf("\t\t\t\t\t\tDo you want to save ? y / n...");   permission = getch();
+            printf("\t\t\t\t\t\tDo you want to save ? y / n...");
+            permission = getch();
             if(permission == 'Y' || permission == 'y')
             {
                 f = fopen("text/studentLogin.txt","a");
                 fprintf(f,"%d %s\n",id,pass);
                 fclose(f);
 
-                for (k=0; k<100; k++)   {   printf("\b");   }
+                for (k=0; k<100; k++)
+                {
+                    printf("\b");
+                }
                 printf("\n\t\t\t\t\t\t*** Account has been created ***\n");
                 printf("\n\t\t\t\t\t\tPress Any Key To LOGIN...");
-                getch();    system("CLS");    login();
+                getch();
+                system("CLS");
+                login();
             }
             else
             {
-              system("CLS");    login();
+                system("CLS");
+                login();
             }
         }
-    }else
+    }
+    else
     {
         printf("\n\t\t\t\t\t-----------------------------------------------\n\n");
-        printf("\t\t\t\t\t!!! Incorrect ID !!!, Try Again ? y / n : "); permission = getch();
+        printf("\t\t\t\t\t!!! Incorrect ID !!!, Try Again ? y / n : ");
+        permission = getch();
 
         if(permission == 'Y' || permission == 'y')
         {
-            system("CLS");    login();
+            system("CLS");
+            login();
         }
         else
             exit(1);
